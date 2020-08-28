b36ffe112ce64c22a36e9ce36b513e6a9f01db8d
Dailton Bastos <dailtonbastos@gmail.com>
2020-08-28 15:12:17 -0300

    deletando itens do carrinho

--- src/libs/cart.js
+++ src/libs/cart.js
@@ -19,7 +19,7 @@ const Cart = {
 
   addOne(product) {
     // check if product already exists in Cart
-    let inCart = this.items.find((item) => item.product.id === product.id)
+    let inCart = this.getCardItem(product.id)
 
     if (!inCart) {
       inCart = {
@@ -51,7 +51,7 @@ const Cart = {
   },
 
   removeOne(productId) {
-    const inCart = this.items.find((item) => item.product.id === productId)
+    const inCart = this.getCardItem(productId)
 
     if (!inCart) return this
 
@@ -72,39 +72,27 @@ const Cart = {
     return this
   },
 
-  delete(productId) {},
+  delete(productId) {
+    const inCart = this.getCardItem(productId)
+
+    if (!inCart) return this
+
+    if (this.items.length > 0) {
+      this.total.quantity -= inCart.quantity
+      this.total.price -= inCart.product.price * inCart.quantity
+      this.total.formattedPrice = formatPrice(this.total.price)
+    }
+
+    this.items = this.items.filter((item) => {
+      return inCart.product.id !== item.product.id
+    })
+
+    return this
+  },
+
+  getCardItem(productId) {
+    return this.items.find((item) => item.product.id === productId)
+  },
 }
-
-const product = {
-  id: 1,
-  price: 199,
-  quantity: 2,
-}
-
-const product2 = {
-  id: 2,
-  price: 299,
-  quantity: 1,
-}
-
-console.log('add first cart item')
-let oldCart = Cart.init().addOne(product)
-console.log(oldCart)
-
-console.log('add second cart item')
-oldCart = Cart.init(oldCart).addOne(product)
-console.log(oldCart)
-
-console.log('add third cart item')
-oldCart = Cart.init(oldCart).addOne(product2)
-console.log(oldCart)
-
-console.log('remove one item')
-oldCart = Cart.init(oldCart).removeOne(product.id)
-console.log(oldCart)
-
-console.log('remove one item again')
-oldCart = Cart.init(oldCart).removeOne(product.id)
-console.log(oldCart)
 
 module.exports = Cart
